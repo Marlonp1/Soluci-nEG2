7981fa3bc12f3744b0f8c431ff1a5dd235962d75
Marlon <60829809@continental.edu.pe>
2024-05-15 18:40:03 -0500

    Se implementó el testeo de cantidades de casos de la ecuación

--- test/TestEcuacionSegundoGrado.py
+++ test/TestEcuacionSegundoGrado.py
@@ -10,18 +10,36 @@ class TestEcuacionSegundoGrado(unittest.TestCase):
         self.calculoRaices = None
 
     def test_calculoESG_dosNumeros_retornaSolucion(self):
-
         # Arrange
         a = 3
         b = -5
         c = 1
-        retultadoEsperadoRaiz1 = 1.43
-        retultadoEsperadoRaiz2 = 0.23
+        resultadoEsperadoRaiz1 = 1.43
+        resultadoEsperadoRaiz2 = 0.23
         # Act
-        resultadoActualRaiz1, resultadoActualRaiz2 = self.calculoRaices.calculoESG(a,b,c)
+        resultadoActualRaiz1, resultadoActualRaiz2 = self.calculoRaices.calculoESG(a, b, c)
         # Assert
-        self.assertAlmostEqual(resultadoActualRaiz1,retultadoEsperadoRaiz1, places=2)
-        self.assertAlmostEqual(resultadoActualRaiz2,retultadoEsperadoRaiz2, places=2)
+        self.assertAlmostEqual(resultadoActualRaiz1, resultadoEsperadoRaiz1, places=2)
+        self.assertAlmostEqual(resultadoActualRaiz2, resultadoEsperadoRaiz2, places=2)
+
+    def test_calculoESG_DiccionarioDosNumeros_retornaSolucion(self):
+        items = (
+            {"Case": "Caso 01", "a": 3, "b": -5, "c": 1, "RaizEsperada1": 1.43, "RaizEsperada2": 0.23},
+            {"Case": "Caso 02", "a": 1, "b": 2, "c": 1, "RaizEsperada1": -1.00, "RaizEsperada2": -1.00},
+            {"Case": "Caso 03", "a": -1, "b": 2, "c": -1, "RaizEsperada1": 1.0, "RaizEsperada2": 1.00},
+            {"Case": "Caso 04", "a": 1, "b": 0, "c": -18, "RaizEsperada1": 4.2, "RaizEsperada2": -4.24},
+            {"Case": "Caso 05", "a": 1, "b": 4, "c": 0, "RaizEsperada1": 0.0, "RaizEsperada2": -4.00},
+            {"Case": "Caso 06", "a": 1, "b": 4, "c": 4, "RaizEsperada1": -2.0, "RaizEsperada2": -2.00},
+            {"Case": "Caso 07", "a": 1, "b": 3, "c": 2, "RaizEsperada1": -1.0, "RaizEsperada2": -2.00},
+        )
+
+        for item in items:
+            with self.subTest(item["Case"]):
+                resultadoEsperadoRaiz1, resultadoEsperadoRaiz2 = self.calculoRaices.calculoESG(item["a"], item["b"], item["c"])
+                resultadoActualRaiz1 = item["RaizEsperada1"]
+                resultadoActualRaiz2 = item["RaizEsperada2"]
+                self.assertAlmostEqual(resultadoEsperadoRaiz1, resultadoActualRaiz1, places=2)
+                self.assertAlmostEqual(resultadoEsperadoRaiz2, resultadoActualRaiz2, places=2)
 
 if __name__ == '__main__':
     unittest.main()
