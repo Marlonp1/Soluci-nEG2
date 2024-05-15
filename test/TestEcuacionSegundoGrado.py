258675e5b4a8dea5c92aa111fe0312f3b27e46de
Marlon <60829809@continental.edu.pe>
2024-05-15 18:46:56 -0500

    Se solucionó el error del caso 4, RaizEsperada1 = 4.24 xd

--- test/TestEcuacionSegundoGrado.py
+++ test/TestEcuacionSegundoGrado.py
@@ -27,7 +27,7 @@ class TestEcuacionSegundoGrado(unittest.TestCase):
             {"Case": "Caso 01", "a": 3, "b": -5, "c": 1, "RaizEsperada1": 1.43, "RaizEsperada2": 0.23},
             {"Case": "Caso 02", "a": 1, "b": 2, "c": 1, "RaizEsperada1": -1.00, "RaizEsperada2": -1.00},
             {"Case": "Caso 03", "a": -1, "b": 2, "c": -1, "RaizEsperada1": 1.0, "RaizEsperada2": 1.00},
-            {"Case": "Caso 04", "a": 1, "b": 0, "c": -18, "RaizEsperada1": 4.2, "RaizEsperada2": -4.24},
+            {"Case": "Caso 04", "a": 1, "b": 0, "c": -18, "RaizEsperada1": 4.24, "RaizEsperada2": -4.24},
             {"Case": "Caso 05", "a": 1, "b": 4, "c": 0, "RaizEsperada1": 0.0, "RaizEsperada2": -4.00},
             {"Case": "Caso 06", "a": 1, "b": 4, "c": 4, "RaizEsperada1": -2.0, "RaizEsperada2": -2.00},
             {"Case": "Caso 07", "a": 1, "b": 3, "c": 2, "RaizEsperada1": -1.0, "RaizEsperada2": -2.00},
